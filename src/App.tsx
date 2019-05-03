dbde0df1056ad06e907236f4d769ffafcc4e86ea
Chris Neven <chrisneven@MacBook-Pro-van-Chris.local>
2019-05-03 10:29:47 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import gql from 'graphql-tag';
+import { useRecipesQuery } from './generated/graphql';
+
+const recipeQuery = gql`
+    query Recipes {
+        recipe {
+            id
+            description
+            name
+            photo_url
+            url
+            recipe_ingredients {
+                ingredient {
+                    name
+                    unit
+                }
+                amount
+            }
+        }
+    }
+`;
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const { data, error, loading } = useRecipesQuery();
+    if (error || loading) {
+        return null;
+    }
+    console.log(data);
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient from 'apollo-boost';
+import App from './App';
+import { ApolloProvider } from 'react-apollo-hooks';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+    uri: 'https://recipeek-server.herokuapp.com/v1alpha1/graphql',
+    headers: { 'x-hasura-admin-secret': 'renevandergijp' }
+});
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
